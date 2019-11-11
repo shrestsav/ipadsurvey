5789731f624178bc96e3df5d463da6550b12d5e2
shrestsav <shrestsav@gmail.com>
2019-11-11 10:38:51 +0545

    Questions updated

--- config/survey.php
+++ config/survey.php
@@ -50,7 +50,7 @@ return [
     'q' => 'Is the government taking steps to improve employee preparedness ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -121,7 +121,7 @@ return [
     'q' => 'Is the government taking steps to improve government technology preparedness ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -192,7 +192,7 @@ return [
     'q' => 'Is the government taking steps to improve regulatory preparedness ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -263,7 +263,7 @@ return [
     'q' => 'Is the government taking steps to improve foundational literacy training ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -334,7 +334,7 @@ return [
     'q' => 'Is the government taking steps to improve core competency  training ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -405,7 +405,7 @@ return [
     'q' => 'Is the government taking steps to improve character qualities training ?',
     'ans' => 
     array (
-      'type' => 'single',
+      'type' => 'single-break',
       'opt' => 
       array (
         0 => 
@@ -430,6 +430,137 @@ return [
   ),
   12 => 
   array (
+    'q' => 'Indicate the sectors most likely to experience a net positive impact from AI. (Choose all that apply.)',
+    'ans' => 
+    array (
+      'type' => 'multipleWithOtherOpt',
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 'Agriculture',
+          'value' => 'Agriculture',
+        ),
+        1 => 
+        array (
+          'key' => 'Art and Design',
+          'value' => 'Art and Design',
+        ),
+        2 => 
+        array (
+          'key' => 'Automotive and Transportation',
+          'value' => 'Automotive and Transportation',
+        ),
+        3 => 
+        array (
+          'key' => 'Aviation and Space',
+          'value' => 'Aviation and Space',
+        ),
+        4 => 
+        array (
+          'key' => 'Chemistry',
+          'value' => 'Chemistry',
+        ),
+        5 => 
+        array (
+          'key' => 'Construction',
+          'value' => 'Construction',
+        ),
+        6 => 
+        array (
+          'key' => 'Education',
+          'value' => 'Education',
+        ),
+        7 => 
+        array (
+          'key' => 'Energy, Oil and Gas',
+          'value' => 'Energy, Oil and Gas',
+        ),
+        8 => 
+        array (
+          'key' => 'Finance and Insurance',
+          'value' => 'Finance and Insurance',
+        ),
+        9 => 
+        array (
+          'key' => 'Government',
+          'value' => 'Government',
+        ),
+        10 => 
+        array (
+          'key' => 'Healthcare',
+          'value' => 'Healthcare',
+        ),
+        11 => 
+        array (
+          'key' => 'Hospitality',
+          'value' => 'Hospitality',
+        ),
+        12 => 
+        array (
+          'key' => 'ICT',
+          'value' => 'ICT',
+        ),
+        13 => 
+        array (
+          'key' => 'Manufacturing',
+          'value' => 'Manufacturing',
+        ),
+        14 => 
+        array (
+          'key' => 'Logistics and Supply Chain',
+          'value' => 'Logistics and Supply Chain',
+        ),
+        15 => 
+        array (
+          'key' => 'Real Estate',
+          'value' => 'Real Estate',
+        ),
+        16 => 
+        array (
+          'key' => 'Tourism',
+          'value' => 'Tourism',
+        ),
+        17 => 
+        array (
+          'key' => 'others',
+          'value' => '',
+        ),
+      ),
+    ),
+    'section' => 2,
+    'alphaKey' => 'a',
+  ),
+  13 => 
+  array (
+    'q' => 'Is your government taking steps to amplify positive impact?',
+    'ans' => 
+    array (
+      'type' => 'single-break',
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 'Yes',
+          'value' => 'Yes: We have (working on) programs in place.',
+        ),
+        1 => 
+        array (
+          'key' => 'No',
+          'value' => 'No: We do not have programs in place.',
+        ),
+        2 => 
+        array (
+          'key' => 'Unsure',
+          'value' => 'Unsure',
+        ),
+      ),
+    ),
+    'section' => 2,
+    'alphaKey' => 'b',
+  ),
+  14 => 
+  array (
     'q' => 'Not all AI impacts are positive. Please indicate the sectors most likely to see a net negative impact from AI. (Choose all that apply.)',
     'ans' => 
     array (
@@ -448,77 +579,176 @@ return [
         ),
         2 => 
         array (
-          'key' => 'others',
-          'value' => '',
-        ),
-        3 => 
-        array (
           'key' => 'Automotive and Transportation',
           'value' => 'Automotive and Transportation',
         ),
-        4 => 
+        3 => 
         array (
           'key' => 'Aviation and Space',
           'value' => 'Aviation and Space',
         ),
-        5 => 
+        4 => 
         array (
           'key' => 'Chemistry',
           'value' => 'Chemistry',
         ),
-        6 => 
+        5 => 
         array (
           'key' => 'Construction',
           'value' => 'Construction',
         ),
-        7 => 
+        6 => 
         array (
           'key' => 'Education',
           'value' => 'Education',
         ),
-        8 => 
+        7 => 
         array (
           'key' => 'Energy, Oil and Gas',
           'value' => 'Energy, Oil and Gas',
         ),
-        9 => 
+        8 => 
         array (
           'key' => 'Finance and Insurance',
           'value' => 'Finance and Insurance',
         ),
-        10 => 
+        9 => 
         array (
           'key' => 'Government',
           'value' => 'Government',
         ),
-        11 => 
+        10 => 
         array (
           'key' => 'Healthcare',
           'value' => 'Healthcare',
         ),
-        12 => 
+        11 => 
         array (
           'key' => 'Hospitality',
           'value' => 'Hospitality',
         ),
-        13 => 
+        12 => 
         array (
           'key' => 'ICT',
           'value' => 'ICT',
         ),
-        14 => 
+        13 => 
         array (
           'key' => 'Manufacturing',
           'value' => 'Manufacturing',
         ),
+        14 => 
+        array (
+          'key' => 'Logistics and Supply Chain',
+          'value' => 'Logistics and Supply Chain',
+        ),
+        15 => 
+        array (
+          'key' => 'Real Estate',
+          'value' => 'Real Estate',
+        ),
+        16 => 
+        array (
+          'key' => 'Tourism',
+          'value' => 'Tourism',
+        ),
+        17 => 
+        array (
+          'key' => 'others',
+          'value' => '',
+        ),
       ),
     ),
     'section' => 2,
+    'alphaKey' => 'c',
+  ),
+  15 => 
+  array (
+    'q' => 'Is your government taking steps to mitigate potential negative impact from AI?',
+    'ans' => 
+    array (
+      'type' => 'single-break',
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 'Yes',
+          'value' => 'Yes: We have (working on) programs in place.',
+        ),
+        1 => 
+        array (
+          'key' => 'No',
+          'value' => 'No: We do not have programs in place.',
+        ),
+        2 => 
+        array (
+          'key' => 'Unsure',
+          'value' => 'Unsure',
+        ),
+      ),
+    ),
+    'section' => 2,
+    'alphaKey' => 'd',
+  ),
+  16 => 
+  array (
+    'q' => 'Does your country have a stated policy or approach to AI ethics?',
+    'ans' => 
+    array (
+      'type' => 'single-break',
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 'Yes',
+          'value' => 'Yes: We have (working on) an AI ethics policy.',
+        ),
+        1 => 
+        array (
+          'key' => 'No',
+          'value' => 'No: We do not have an AI ethics policy.',
+        ),
+        2 => 
+        array (
+          'key' => 'Unsure',
+          'value' => 'Unsure',
+        ),
+      ),
+    ),
+    'section' => 3,
     'alphaKey' => 'a',
   ),
-  13 => 
+  17 => 
   array (
-    'q' => '. Which of the following characteristics should be included in a Universal AI Ethics code? (Choose all that apply.)',
+    'q' => 'Do you believe there is a need for a Universal AI Ethics code?',
+    'ans' => 
+    array (
+      'type' => 'single-break',
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 'Yes',
+          'value' => 'Yes: AI Ethics should be defined at an international level.',
+        ),
+        1 => 
+        array (
+          'key' => 'No',
+          'value' => 'No: AI Ethics should be defined at a national or regional level.',
+        ),
+        2 => 
+        array (
+          'key' => 'Unsure',
+          'value' => 'Unsure',
+        ),
+      ),
+    ),
+    'section' => 3,
+    'alphaKey' => 'b',
+  ),
+  18 => 
+  array (
+    'q' => 'Which of the following characteristics should be included in a Universal AI Ethics code? (Choose all that apply.)',
     'ans' => 
     array (
       'type' => 'multiple',
@@ -551,124 +781,10 @@ return [
         ),
       ),
     ),
-    'section' => 2,
+    'section' => 3,
     'alphaKey' => 'c',
   ),
-  14 => 
-  array (
-    'q' => 'How was your overall experience?',
-    'ans' => 
-    array (
-      'type' => 'rating',
-      'label' => 
-      array (
-        0 => 'UnEffective',
-        1 => 'Effective',
-      ),
-      'opt' => 
-      array (
-        0 => 
-        array (
-          'key' => 1,
-          'value' => 1,
-        ),
-        1 => 
-        array (
-          'key' => 2,
-          'value' => 2,
-        ),
-        2 => 
-        array (
-          'key' => 3,
-          'value' => 3,
-        ),
-        3 => 
-        array (
-          'key' => 4,
-          'value' => 4,
-        ),
-        4 => 
-        array (
-          'key' => 5,
-          'value' => 5,
-        ),
-      ),
-    ),
-    'section' => 2,
-    'alphaKey' => 'a',
-  ),
-  15 => 
-  array (
-    'q' => 'Do you like it?',
-    'ans' => 
-    array (
-      'type' => 'rating',
-      'label' => 
-      array (
-        0 => 'UnEffective',
-        1 => 'Effective',
-      ),
-      'opt' => 
-      array (
-        0 => 
-        array (
-          'key' => 1,
-          'value' => 1,
-        ),
-        1 => 
-        array (
-          'key' => 2,
-          'value' => 2,
-        ),
-        2 => 
-        array (
-          'key' => 3,
-          'value' => 3,
-        ),
-        3 => 
-        array (
-          'key' => 4,
-          'value' => 4,
-        ),
-        4 => 
-        array (
-          'key' => 5,
-          'value' => 5,
-        ),
-      ),
-    ),
-    'section' => 2,
-    'alphaKey' => 'b',
-  ),
-  16 => 
-  array (
-    'q' => 'Indicate the sectors most likely to experience a net positive impact from AI',
-    'ans' => 
-    array (
-      'type' => 'otherOptions',
-      'opt' => 
-      array (
-        0 => 
-        array (
-          'key' => 'Data',
-          'value' => 'Data Privacy & Cybersecurity',
-        ),
-        1 => 
-        array (
-          'key' => 'Blockchain',
-          'value' => 'Blockchain & Digital Currency',
-        ),
-        2 => 
-        array (
-          'key' => 'others',
-          'value' => '',
-        ),
-      ),
-    ),
-    'section' => 2,
-    'alphaKey' => 'c',
-  ),
-  17 => 
+  19 => 
   array (
     'q' => 'What topic would you be most interested in discussing next?',
     'ans' => 
@@ -686,14 +802,184 @@ return [
     'section' => 4,
     'alphaKey' => 'a',
   ),
+  20 => 
+  array (
+    'q' => 'How was your overall experience?',
+    'ans' => 
+    array (
+      'type' => 'rating',
+      'label' => 
+      array (
+        0 => 'Below expectations',
+        1 => 'Exceeded expectations',
+      ),
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 1,
+          'value' => 1,
+        ),
+        1 => 
+        array (
+          'key' => 2,
+          'value' => 2,
+        ),
+        2 => 
+        array (
+          'key' => 3,
+          'value' => 3,
+        ),
+        3 => 
+        array (
+          'key' => 4,
+          'value' => 4,
+        ),
+        4 => 
+        array (
+          'key' => 5,
+          'value' => 5,
+        ),
+      ),
+    ),
+    'section' => 5,
+    'alphaKey' => 'a',
+  ),
+  21 => 
+  array (
+    'q' => 'How would you rate today’s venue?',
+    'ans' => 
+    array (
+      'type' => 'rating',
+      'label' => 
+      array (
+        0 => 'Below expectations',
+        1 => 'Exceeded expectations',
+      ),
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 1,
+          'value' => 1,
+        ),
+        1 => 
+        array (
+          'key' => 2,
+          'value' => 2,
+        ),
+        2 => 
+        array (
+          'key' => 3,
+          'value' => 3,
+        ),
+        3 => 
+        array (
+          'key' => 4,
+          'value' => 4,
+        ),
+        4 => 
+        array (
+          'key' => 5,
+          'value' => 5,
+        ),
+      ),
+    ),
+    'section' => 5,
+    'alphaKey' => 'b',
+  ),
+  22 => 
+  array (
+    'q' => 'How engaging was today’s conversation?',
+    'ans' => 
+    array (
+      'type' => 'rating',
+      'label' => 
+      array (
+        0 => 'Below expectations',
+        1 => 'Exceeded expectations',
+      ),
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 1,
+          'value' => 1,
+        ),
+        1 => 
+        array (
+          'key' => 2,
+          'value' => 2,
+        ),
+        2 => 
+        array (
+          'key' => 3,
+          'value' => 3,
+        ),
+        3 => 
+        array (
+          'key' => 4,
+          'value' => 4,
+        ),
+        4 => 
+        array (
+          'key' => 5,
+          'value' => 5,
+        ),
+      ),
+    ),
+    'section' => 5,
+    'alphaKey' => 'c',
+  ),
+  23 => 
+  array (
+    'q' => 'How would you rate today’s moderator?',
+    'ans' => 
+    array (
+      'type' => 'rating',
+      'label' => 
+      array (
+        0 => 'Below expectations',
+        1 => 'Exceeded expectations',
+      ),
+      'opt' => 
+      array (
+        0 => 
+        array (
+          'key' => 1,
+          'value' => 1,
+        ),
+        1 => 
+        array (
+          'key' => 2,
+          'value' => 2,
+        ),
+        2 => 
+        array (
+          'key' => 3,
+          'value' => 3,
+        ),
+        3 => 
+        array (
+          'key' => 4,
+          'value' => 4,
+        ),
+        4 => 
+        array (
+          'key' => 5,
+          'value' => 5,
+        ),
+      ),
+    ),
+    'section' => 5,
+    'alphaKey' => 'd',
+  ),
 ),
 
     'sections' => array (
   0 => 
   array (
-    'title' => 'AI is advancing at an astonishing speed. Is the government ready
-      to usher in the benefits of AI for society ? Consider your own
-      nation\'s context for the coming 3 years.',
+    'title' => 'AI is advancing at an astonishing speed. Is the government ready to usher in the benefits of AI for society ? Consider your own nation\'s context for the coming 3 years.',
   ),
   1 => 
   array (
@@ -705,12 +991,11 @@ return [
   ),
   3 => 
   array (
-    'title' => 'The widespread adoption of AI raises significant regulatory concerns for governments, particularly in scenarios where an AI algorithm is used to inform decisions that have direct or indirect impact on the lives of citizens. The following questions pertain to the ethics of AI. ',
+    'title' => 'The widespread adoption of AI raises significant regulatory concerns for governments, particularly in scenarios where an AI algorithm is used to inform decisions that have direct or indirect impact on the lives of citizens. The following questions pertain to the ethics of AI.',
   ),
   4 => 
   array (
-    'title' => 'Digital Majlis is a gathering of regional and international thought leaders and government officials to discuss the most urgent questions facing government during the 4th Industrial Revolution. 
-      What topic would you be most interested in discussing next?',
+    'title' => 'Digital Majlis is a gathering of regional and international thought leaders and government officials to discuss the most urgent questions facing government during the 4th Industrial Revolution. ',
   ),
   5 => 
   array (
